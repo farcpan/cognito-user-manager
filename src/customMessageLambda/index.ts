4c2bc5fd5f93fcea7a0c3edc93d454e66534cde1
main <main@20240626-my-network.local>
2024-08-11 15:53:39 +0900

    ユーザープール作成

---
+++ src/customMessageLambda/index.ts
@@ -0,0 +1,58 @@
+/////////////////////////////////////////////////////////////////////////////////
+// カスタムメッセージLambda
+/////////////////////////////////////////////////////////////////////////////////
+/**
+ * パスワードを忘れた場合に送信するメッセージを取得する
+ *
+ * @returns パスワードリセットメッセージ
+ */
+const getPasswordForgotMessage = () => {
+	return (
+		'<html>' +
+		'<head></head>' +
+		'<body>' +
+		'<p>確認コード: {####} </p>' +
+		'<br/>' +
+		'<p>※有効期限は30分です。ご注意ください。</p>' +
+		'</body>' +
+		'</html>'
+	);
+};
+
+//
+/**
+ * アカウント作成時の検証メッセージを取得する
+ *
+ * @returns アカウント作成メッセージ
+ */
+const getVerificationMessage = () => {
+	return (
+		'<html>' +
+		'<head></head>' +
+		'<body>' +
+		'<p>アカウント登録ありがとうございます。下記確認コードを入力し、アカウント登録を完了してください。</p>' +
+		'<br/>' +
+		'<p>確認コード：{####} </p>' +
+		'<br/>' +
+		'</body>' +
+		'</html>'
+	);
+};
+
+/**
+ * Cognitoカスタムメッセージ送信Handler
+ *
+ * @param event Lambdaイベント
+ * @param context Lambdaコンテキスト
+ * @returns Cognitoカスタムメッセージ送信結果
+ */
+export const handler = async (event: any, context: any) => {
+	if (event.triggerSource === 'CustomMessage_SignUp') {
+		event.response.emailSubject = '本登録用検証コード';
+		event.response.emailMessage = getVerificationMessage();
+	} else if (event.triggerSource === 'CustomMessage_ForgotPassword') {
+		event.response.emailSubject = 'パスワードリセット用検証コード';
+		event.response.emailMessage = getPasswordForgotMessage();
+	}
+	return event;
+};
--- src/lib/main-stack.ts
+++ src/lib/main-stack.ts
@@ -1,7 +1,12 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { join } from 'path';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 import { ContextParameters } from '../utils/context';
+import { AccountRecovery, UserPool, UserPoolEmail } from 'aws-cdk-lib/aws-cognito';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 
 interface MainStackProps extends StackProps {
 	context: ContextParameters;
@@ -11,6 +16,84 @@ export class MainStack extends Stack {
 	constructor(scope: Construct, id: string, props: MainStackProps) {
 		super(scope, id, props);
 
-		//
+		// カスタムメッセージLambdaトリガー用Lambda
+		const customMessageLambdaPath = join(__dirname, '../customMessageLambda/index.ts');
+		const customMessageLambdaName = props.context.getResourceId('custom-message-func');
+		const lambdaFunction = new NodejsFunction(this, customMessageLambdaName, {
+			functionName: customMessageLambdaName,
+			runtime: Runtime.NODEJS_LATEST,
+			entry: customMessageLambdaPath,
+			handler: 'handler',
+			logRetention: RetentionDays.ONE_DAY,
+		});
+
+		// Cognito
+		const userPoolName = props.context.getResourceId('user-pool');
+		const userPool = new UserPool(this, userPoolName, {
+			userPoolName: userPoolName,
+
+			// アカウント復元手段
+			accountRecovery: AccountRecovery.EMAIL_ONLY,
+
+			// 検証方法
+			autoVerify: {
+				email: true,
+				phone: false,
+			},
+
+			// アカウント削除保護
+			deletionProtection: false,
+
+			// Cognitoからのメール配信設定
+			email: UserPoolEmail.withCognito(),
+			// SESを経由する場合
+			/*
+			email: cognito.UserPoolEmail.withSES({
+						sesRegion: 'ap-northeast-1', // 必須（無いとエラーになる）
+						fromEmail: props.context.stageParameters.cognito.fromEmail,
+						fromName: props.context.stageParameters.cognito.fromName,
+						replyTo: undefined,
+				  }),
+            */
+
+			// スタック削除時のユーザープール削除設定
+			removalPolicy: RemovalPolicy.DESTROY,
+
+			// ログインパスワードポリシー
+			passwordPolicy: {
+				minLength: 8,
+				requireLowercase: true,
+				requireUppercase: true,
+				requireDigits: true,
+				requireSymbols: true,
+				tempPasswordValidity: Duration.days(1),
+			},
+
+			// ユーザーが自分でアカウント登録できるか？
+			selfSignUpEnabled: true,
+
+			// ユーザーIDの大文字小文字を区別するか？
+			signInCaseSensitive: false,
+
+			// ユーザー登録に必要な要素
+			standardAttributes: {
+				email: { required: true },
+			},
+
+			// ログイン時に利用可能なエイリアス: [!] ユーザープール作成後に変更できない
+			signInAliases: {
+				username: true, // username:trueは必ず指定する必要がある
+				email: true, // メールアドレスもエイリアスとして使用可能とする
+				phone: false,
+			},
+
+			// ユーザー検証方法のオプション: カスタムメッセージを使用するため設定不要
+			userVerification: undefined,
+
+			// カスタムメッセージLambdaトリガー
+			lambdaTriggers: {
+				customMessage: lambdaFunction,
+			},
+		});
 	}
 }
